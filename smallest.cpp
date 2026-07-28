146b6c8eb51e6116f76296474db8ef4712255b11
Syed Ehtesham Ali Raza <ehteshamaliraza@gmail.com>
2026-07-28 10:31:28 +0500

    Implement function to find the smallest of three numbers

---
+++ smallest.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int smallest(int a, int b, int c)
+{
+	if (a <= b && a <= c)
+	{
+		return a;
+	}
+	else if (b <= a && b <= c)
+	{
+		return b;
+	}
+	else
+	{
+		return c;
+	}
+}
+
+int main()
+{
+	int a, b, c;
+	cout << "Enter three numbers : ";
+	cin >> a >> b >> c;
+	
+	cout << "Smallest number = " << smallest(a, b, c);
+}
