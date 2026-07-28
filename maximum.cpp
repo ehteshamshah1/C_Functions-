f676a80d7f44983d60f163d7496285c48d6de478
Syed Ehtesham Ali Raza <ehteshamaliraza@gmail.com>
2026-07-28 10:26:51 +0500

    Implement maximum function to find larger of two numbers

---
+++ maximum.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int maximum(int a, int b)
+{
+    if (a > b)
+    {
+        return a;
+    }
+    else
+    {
+        return b;
+    }
+}
+
+int main()
+{
+    int a, b;
+
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+
+    cout << "Maximum number = " << maximum(a, b);
+
+    return 0;
+}
